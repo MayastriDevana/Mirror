6f1c15f426c5e80354c71d8b90c9c4e9434a5fe4
Maya <Mayastridevana0205@gmail.com>
2024-05-02 09:35:35 +0700

    Initial Commit

---
+++ Mirror/MirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MirrorApp.swift
+//  Mirror
+//
+//  Created by User on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
